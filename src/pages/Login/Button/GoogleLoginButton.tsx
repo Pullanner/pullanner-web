eb8ff3f494d4eb744cbdbfc3e23edabc80f0e7b9
soralee <sora2821@gmail.com>
2023-05-31 17:07:39 +0900

    [#2] Feat: 로그인 버튼 클릭시 '/callback'으로 이동하는 Link 추가

--- src/pages/Login/Button/GoogleLoginButton.tsx
+++ src/pages/Login/Button/GoogleLoginButton.tsx
@@ -1,11 +1,15 @@
+import { Link } from 'react-router-dom';
+
 export const GoogleLoginButton = () => {
   return (
-    <button
-      type="button"
-      className="flex flex-row justify-center items-center w-300 h-10 mb-5 rounded-md bg-white"
-    >
-      <img src="/assets/google-icon.svg" alt="Goole Icon" />
-      <span className="ml-2.5 font-sans font-bold text-sm">Google로 시작하기</span>
-    </button>
+    <Link to="/callback" state={{ platform: 'google' }}>
+      <button
+        type="button"
+        className="flex flex-row justify-center items-center w-300 h-10 mb-5 rounded-md bg-white"
+      >
+        <img src="/assets/google-icon.svg" alt="Goole Icon" />
+        <span className="ml-2.5 font-sans font-bold text-sm">Google로 시작하기</span>
+      </button>
+    </Link>
   );
 };
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -4,7 +4,7 @@ export const Login = () => {
   return (
     <>
       <h1 className="mb-14">
-        <img src="/public/assets/logo.svg" alt="Logo" />
+        <img src="/assets/logo.svg" alt="Logo" />
       </h1>
       <h3 className="mb-9 font-sans text-sm font-normal text-white tracking-tight">
         로그인 후 플래너의 다양한 기능을 사용해보세요
