8720031e6a113ee6c52e1f9c4fe2a4ba1fd91547
soralee <sora2821@gmail.com>
2023-06-02 16:53:21 +0900

    [#2] Feat: add backward button to prev page

--- src/pages/LoginLoading/index.tsx
+++ src/pages/LoginLoading/index.tsx
@@ -1,17 +1,35 @@
 import { accessTokenAtom } from '@/stores/accessTokenAtom';
 import { getCookie } from '@/utils/cookie';
-import { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
+import { useEffect, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
 
 export const LoginLoading = () => {
-  const storeAccessToken = useSetRecoilState(accessTokenAtom);
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenAtom);
+  const navigate = useNavigate();
+  const goBack = useCallback(() => {
+    navigate(-2);
+  }, [navigate]);
 
   useEffect(() => {
-    const accessToken = getCookie('auth');
-    if (accessToken) {
-      storeAccessToken(accessToken);
+    const accessTokenValue = getCookie('accessToken');
+    if (accessTokenValue) {
+      setAccessToken(accessTokenValue);
     }
-  }, [storeAccessToken]);
+  }, [setAccessToken]);
 
-  return <div className="font-sans text-xl text-white">Loading...</div>;
+  return (
+    <>
+      <div className="font-sans text-xl text-white">Loading...</div>
+      {accessToken && (
+        <button
+          type="button"
+          onClick={goBack}
+          className="font-sans text-sm mt-5 px-5 py-2.5 bg-teal-300 rounded"
+        >
+          이전 페이지로 이동하기
+        </button>
+      )}
+    </>
+  );
 };
