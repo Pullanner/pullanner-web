2f27c6bea08372d06bbace251b2bcb5b00bded87
soralee <sora2821@gmail.com>
2023-06-05 00:39:31 +0900

    [#7]Feat: add default instance of axios in lib Co-authored-by: Jayun Lee <jayunlee.dev@gmail.com>

---
+++ src/lib/axios/defaultInstance.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosError, AxiosInstance, InternalAxiosRequestConfig, AxiosResponse } from 'axios';
+
+const defaultInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_UR,
+  timeout: 10000,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+const onRequest = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+  return config;
+};
+
+export const onResponse = (response: AxiosResponse): AxiosResponse => {
+  return response;
+};
+
+const onErrorResponse = async (error: AxiosError | Error): Promise<AxiosError> => {
+  return Promise.reject(error);
+};
+
+const setupInterceptors = (instance: AxiosInstance): AxiosInstance => {
+  instance.interceptors.request.use(onRequest, onErrorResponse);
+  instance.interceptors.response.use(onResponse, onErrorResponse);
+  return instance;
+};
+
+export const defaultInstanceWithInterceptors = setupInterceptors(defaultInstance);
