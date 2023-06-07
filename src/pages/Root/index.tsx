6c1b190bcdd23799a4aee149b63f03a7e93f8cd4
soralee <sora2821@gmail.com>
2023-06-07 20:03:21 +0900

    [#15] Feat: add header component

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,9 +1,11 @@
+import { Header } from '@/components/Header';
 import { Outlet } from 'react-router-dom';
 
 export const Root = () => {
   return (
     <div className="w-screen h-screen flex flex-row justify-center items-center">
-      <div className="w-96 h-mobileHeight flex flex-col justify-center items-center bg-black">
+      <div className="w-96 h-mobileHeight bg-black">
+        <Header />
         <Outlet />
       </div>
     </div>
