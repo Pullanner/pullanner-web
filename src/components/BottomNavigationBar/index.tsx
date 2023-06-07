ee3adbbb9d09e863d284a35c7344662a08f34611
soralee <sora2821@gmail.com>
2023-06-08 02:02:42 +0900

    [#16] Refactor: change border width of bottom navigation bar component

--- src/components/BottomNavigationBar/index.tsx
+++ src/components/BottomNavigationBar/index.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export const BottomNavigationBar = () => {
   return (
-    <nav className="absolute bottom-0 flex justify-around w-96 h-20 border border-white rounded-t-[20px] bg-black">
+    <nav className="absolute bottom-0 flex justify-around w-96 h-20 border-[0.5px] border-white rounded-t-[20px] bg-black">
       <Link to="/" className="flex flex-col justify-center items-center">
         <img className="mb-1.5" src="/assets/roadmap-icon.svg" alt="roadmap" />
         <span className="font-sans text-xs font-medium text-white">Roadmap</span>
