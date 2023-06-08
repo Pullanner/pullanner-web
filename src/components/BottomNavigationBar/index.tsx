d8f8edc810bd71deafb3f4f6908350253867e9d0
soralee <sora2821@gmail.com>
2023-06-08 21:56:23 +0900

    [#16] Refactor: remove duplicate codes by useing IconLink component, ICON_LINK_DATA, and map method

--- src/components/BottomNavigationBar/index.tsx
+++ src/components/BottomNavigationBar/index.tsx
@@ -5,45 +5,61 @@ const NAVLINK_CLASS_VARIANTS = {
   inactive: 'flex flex-col justify-center items-center text-white',
 } as const;
 
+const ICON_LINK_DATA = [
+  {
+    name: 'Roadmap',
+    linkPath: '/',
+    iconClass: 'icon-roadmap-icon',
+  },
+  {
+    name: 'Jounal',
+    linkPath: '/journal',
+    iconClass: 'icon-journal-icon',
+  },
+  {
+    name: 'Dashboard',
+    linkPath: '/dashboard',
+    iconClass: 'icon-dashboard-icon',
+  },
+  {
+    name: 'Community',
+    linkPath: '/community',
+    iconClass: 'icon-community-icon',
+  },
+] as const;
+
+type IconLinkProps = {
+  name: string;
+  linkPath: string;
+  iconClass: string;
+};
+
+type Props = {
+  iconLinkProps: IconLinkProps;
+};
+
+const IconLink = ({ iconLinkProps }: Props) => {
+  const { name, linkPath, iconClass } = iconLinkProps;
+
+  return (
+    <NavLink
+      to={linkPath}
+      className={({ isActive }) => {
+        return isActive ? NAVLINK_CLASS_VARIANTS.active : NAVLINK_CLASS_VARIANTS.inactive;
+      }}
+    >
+      <i className={iconClass} />
+      <span className="mt-1.5 font-sans text-xs font-medium">{name}</span>
+    </NavLink>
+  );
+};
+
 export const BottomNavigationBar = () => {
   return (
     <nav className="absolute bottom-0 flex justify-around w-96 h-20 border border-white rounded-t-[20px] bg-black">
-      <NavLink
-        to="/"
-        className={({ isActive }) => {
-          return isActive ? NAVLINK_CLASS_VARIANTS.active : NAVLINK_CLASS_VARIANTS.inactive;
-        }}
-      >
-        <i className="icon-roadmap-icon" />
-        <span className="mt-1.5 font-sans text-xs font-medium">Roadmap</span>
-      </NavLink>
-      <NavLink
-        to="/journal"
-        className={({ isActive }) => {
-          return isActive ? NAVLINK_CLASS_VARIANTS.active : NAVLINK_CLASS_VARIANTS.inactive;
-        }}
-      >
-        <i className="icon-journal-icon" />
-        <span className="mt-1.5 font-sans text-xs font-medium">Journal</span>
-      </NavLink>
-      <NavLink
-        to="/dashboard"
-        className={({ isActive }) => {
-          return isActive ? NAVLINK_CLASS_VARIANTS.active : NAVLINK_CLASS_VARIANTS.inactive;
-        }}
-      >
-        <i className="icon-dashboard-icon" />
-        <span className="mt-1.5 font-sans text-xs font-medium">Dashboard</span>
-      </NavLink>
-      <NavLink
-        to="/community"
-        className={({ isActive }) => {
-          return isActive ? NAVLINK_CLASS_VARIANTS.active : NAVLINK_CLASS_VARIANTS.inactive;
-        }}
-      >
-        <i className="icon-community-icon" />
-        <span className="mt-1.5 font-sans text-xs font-medium">Community</span>
-      </NavLink>
+      {ICON_LINK_DATA.map((iconLinkData: IconLinkProps) => {
+        return <IconLink key={iconLinkData.name} iconLinkProps={iconLinkData} />;
+      })}
     </nav>
   );
 };
