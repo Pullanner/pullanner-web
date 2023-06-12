3c10d5ad4bbc6676207462f58dd786110abc4197
soralee <sora2821@gmail.com>
2023-06-12 15:49:09 +0900

    [#20] Refactor: update image src including image directory path

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -19,7 +19,7 @@ export const Login = () => {
   return (
     <div className="flex flex-col justify-center items-center my-52">
       <h1 className="mb-14">
-        <img src="/assets/logo.svg" alt="Logo" />
+        <img src="/assets/images/logo.svg" alt="Logo" />
       </h1>
       <h3 className="mb-9 font-sans text-sm font-normal text-white tracking-tight">
         로그인 후 플래너의 다양한 기능을 사용해보세요
