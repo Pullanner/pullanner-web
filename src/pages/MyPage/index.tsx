48b77c44cd545275a04a45e7cd6a1c03d0d8cf10
soralee <sora2821@gmail.com>
2023-06-22 17:23:03 +0900

    [#22] Feat: add MyPage component
    - call useUserData hook in MyPage component
    - declare handler function of logout button

---
+++ src/pages/MyPage/index.tsx
@@ -0,0 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
+
+import { LargeBlockButton } from '@/components/LargeBlockButton';
+import { ProgressSection, UserSection, TabSection } from '@/components/MyPage';
+import { API_PATH, ROUTE_PATH } from '@/constants';
+import { authInstance } from '@/lib/axios/authInstance';
+import { useUserData } from '@/lib/react-query/useUserData';
+import { loginStateAtom } from '@/stores/atoms/loginStateAtom';
+
+export const MyPage = () => {
+  const { data } = useUserData();
+  const setLoginState = useSetRecoilState(loginStateAtom);
+  const navigate = useNavigate();
+
+  const handleLogoutButtonClick = () => {
+    authInstance.delete(API_PATH.token);
+    setLoginState(false);
+    navigate(ROUTE_PATH.root);
+  };
+  return (
+    <div className="flex flex-col justify-center items-center border-t-0">
+      <UserSection profileImage={data?.profileImage} nickName={data?.nickName} />
+      <ProgressSection />
+      <TabSection />
+      <LargeBlockButton name="로그아웃" handler={handleLogoutButtonClick} />
+    </div>
+  );
+};
