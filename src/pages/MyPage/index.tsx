ae25a0579d71fd6401ca34bd3b5ba22b1cadbcb8
soralee <sora2821@gmail.com>
2023-06-24 11:11:41 +0900

    [#26] Refactor: add logic to initialize Access Token atom

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -6,16 +6,19 @@ import { ProgressSection, UserSection, TabSection } from '@/components/MyPage';
 import { API_PATH, ROUTE_PATH } from '@/constants';
 import { authInstance } from '@/lib/axios/authInstance';
 import { useUserData } from '@/lib/react-query/useUserData';
+import { accessTokenAtom } from '@/stores/atoms/accessTokenAtom';
 import { loginStateAtom } from '@/stores/atoms/loginStateAtom';
 
 export const MyPage = () => {
   const { data } = useUserData();
+  const setAccessToken = useSetRecoilState(accessTokenAtom);
   const setLoginState = useSetRecoilState(loginStateAtom);
   const navigate = useNavigate();
 
   const handleLogoutButtonClick = () => {
     authInstance.delete(API_PATH.token);
     setLoginState(false);
+    setAccessToken('');
     navigate(ROUTE_PATH.root);
   };
   return (
