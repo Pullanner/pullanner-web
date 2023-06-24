30457575bf4f0f3fb38dbb983b4082afec0248a9
soralee <sora2821@gmail.com>
2023-06-24 13:42:44 +0900

    [#26] Refactor: modify ProgressSection by using journalCount
    - get percent by useing getPercent function
    - get progressWidth by using getPercentWidth function
    - calculate the rest journalCount in order to level-up
    - show current journalCount and maximum journalCount in current level

--- src/components/MyPage/ProgressSection/index.tsx
+++ src/components/MyPage/ProgressSection/index.tsx
@@ -1,14 +1,22 @@
-// TODO: API 명세에 따라 percentageWidth, span 내용들 변경
-export const ProgressSection = () => {
-  const percentageWidth = 'w-[20%]';
+import { MAX_JOURNAL_COUNT_BY_LEVEL } from '@/constants';
+import { getPercent, getPercentWidth } from '@/utils/percent';
+
+type ProgressSectionProps = {
+  journalCount: number;
+  level: keyof typeof MAX_JOURNAL_COUNT_BY_LEVEL;
+};
+
+export const ProgressSection = ({ journalCount, level }: ProgressSectionProps) => {
+  const percent = getPercent(journalCount, MAX_JOURNAL_COUNT_BY_LEVEL[level]);
+  const progressWidth = getPercentWidth(percent);
   return (
     <section className="w-full px-6 ">
       <div className="h-2.5 bg-gray-200 rounded-full">
-        <div className={`${percentageWidth} h-2.5 bg-primary rounded-full`} />
+        <div className={`${progressWidth} h-2.5 bg-primary rounded-full`} />
       </div>
       <div className="flex mt-2 justify-between text-xs text-white">
-        <span>저널 80개만 더 작성하면 레벨업</span>
-        <span>20/100</span>
+        <span>저널 {MAX_JOURNAL_COUNT_BY_LEVEL[level] - journalCount}개만 더 작성하면 레벨업</span>
+        <span>{`${journalCount} / ${MAX_JOURNAL_COUNT_BY_LEVEL[level]}`}</span>
       </div>
     </section>
   );
