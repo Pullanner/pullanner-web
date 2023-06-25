a6479e4ca37d3084be467729bf87a5e741e041f4
soralee <sora2821@gmail.com>
2023-06-25 23:32:39 +0900

    [#26] Refactor: use style proprety instead of tailwindCSS in progress bar

--- src/components/MyPage/ProgressSection/index.tsx
+++ src/components/MyPage/ProgressSection/index.tsx
@@ -1,5 +1,5 @@
 import { MAX_JOURNAL_COUNT_BY_LEVEL } from '@/constants';
-import { getPercent, getPercentWidth } from '@/utils/percent';
+import { getPercent } from '@/utils/percent';
 
 type ProgressSectionProps = {
   journalCount: number | null;
@@ -10,11 +10,10 @@ export const ProgressSection = ({ journalCount, level }: ProgressSectionProps) =
   const currentJournalCount = journalCount || 0;
   const currentLevel = level || 1;
   const percent = getPercent(currentJournalCount, MAX_JOURNAL_COUNT_BY_LEVEL[currentLevel]);
-  const progressWidth = getPercentWidth(percent);
   return (
     <section className="w-full px-6 ">
       <div className="h-2.5 bg-gray-200 rounded-full">
-        <div className={`${progressWidth} h-2.5 bg-primary rounded-full`} />
+        <div className="h-2.5 bg-primary rounded-full" style={{ width: `${percent}%` }} />
       </div>
       <div className="flex mt-2 justify-between text-xs text-white">
         <span>
