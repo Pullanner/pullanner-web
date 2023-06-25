5db21505f33b9f3ba5551d5aabb0fe5902741194
soralee <sora2821@gmail.com>
2023-06-25 23:46:23 +0900

    [#26] Refactor: declare remainingJournalCount variable rather than use formula

--- src/components/MyPage/ProgressSection/index.tsx
+++ src/components/MyPage/ProgressSection/index.tsx
@@ -11,14 +11,13 @@ export const ProgressSection = ({ journalCount, level }: ProgressSectionProps) =
   const currentJournalCount = journalCount || 0;
   const currentLevel = level || 1;
   const percent = getPercent(currentJournalCount, MAX_JOURNAL_COUNT_BY_LEVEL[currentLevel]);
+  const remainingJournalCount = MAX_JOURNAL_COUNT_BY_LEVEL[currentLevel] - currentJournalCount;
+
   return (
     <section className="w-full px-6 ">
       <ProgressBar percent={percent} />
       <div className="flex mt-2 justify-between text-xs text-white">
-        <span>
-          저널 {MAX_JOURNAL_COUNT_BY_LEVEL[currentLevel] - currentJournalCount}개만 더 작성하면
-          레벨업
-        </span>
+        <span>{`저널 ${remainingJournalCount}개만 더 작성하면 레벨업`}</span>
         <span>{`${currentJournalCount} / ${MAX_JOURNAL_COUNT_BY_LEVEL[currentLevel]}`}</span>
       </div>
     </section>
