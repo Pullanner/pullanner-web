b6f48c419c64b8d272bea7ff273cbe87044b1f4c
Jayun <jay.lee.2280@gmail.com>
2023-06-28 16:53:38 +0900

    [#32] Design: modify width for responsive design
    Co-authored-by: soralee2821 <soralee2821@users.noreply.github.com>

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -6,7 +6,7 @@ import { Header } from '@/components/Header';
 export const Root = () => {
   return (
     <div className="w-screen h-screen flex flex-row justify-center items-center">
-      <div className="relative w-96 h-[100%] sm:h-[50rem] sm:max-h-[90vh] bg-black">
+      <div className="relative w-[100%] sm:w-96 h-[100%] sm:h-[50rem] sm:max-h-[90vh] bg-black">
         <Header />
         <Outlet />
         <BottomNavigationBar />
