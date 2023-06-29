c22ce83e07f153e9bb963f25589cf9c8eeb10314
Jayun <jay.lee.2280@gmail.com>
2023-06-29 12:26:57 +0900

    [#32] Design: add `overflow-y: auto` to Outlet wrapper for scroll
    - Outlet의 컨텐츠들이 많아져서 길어질 시 스크롤이 될 수 있도록 `overflow-y: auto`를 적용함
    - parent element box를 기준으로 설정해야 하기 때문에 하위에 div 태그를 추가함

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -8,8 +8,10 @@ export const Root = () => {
     <div className="w-screen h-screen flex flex-row justify-center items-center">
       <div className="w-[100%] sm:w-96 h-[100%] sm:h-[50rem] sm:max-h-[90vh] bg-black">
         <Header />
-        <div className="flex flex-col justify-center items-center h-[calc(100%-8rem)]">
-          <Outlet />
+        <div className="flex flex-col items-center h-[calc(100%-8rem)]  py-2.5">
+          <div className="overflow-y-auto">
+            <Outlet />
+          </div>
         </div>
         <BottomNavigationBar />
       </div>
