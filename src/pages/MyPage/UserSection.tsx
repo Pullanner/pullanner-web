b3fd42063a0eaa4f653bf2c91c181ff093c04ea6
soralee <sora2821@gmail.com>
2023-06-30 23:45:20 +0900

    [#26] Refactor: separate EditButton to UserSection for code readability

--- src/pages/MyPage/UserSection.tsx
+++ src/pages/MyPage/UserSection.tsx
@@ -9,19 +9,25 @@ type UserSectionProps = {
   email: string;
 };
 
+const MyPageEditButton = () => {
+  return (
+    <Link
+      to={ROUTE_PATH.myPage.edit}
+      className="flex w-[6.25rem] h-7 border border-white rounded-[1.25rem] items-center"
+    >
+      <span className="px-3 text-xs text-white">정보수정</span>
+      <img src="/assets/images/edit-icon.svg" alt="editIcon" />
+    </Link>
+  );
+};
+
 export const UserSection = ({ profileImage, nickName, email }: UserSectionProps) => {
   return (
     <section className="pt-12 pb-7 flex flex-col justify-center items-center">
       <ProfileImage url={profileImage} size="6rem" />
       <span className="inline-block pb-[0.313rem]  text-main font-semibold text-white">{`안녕하세요, ${nickName}님`}</span>
       <span className="inline-block pb-[1.125rem] text-xs text-gray-400">{email}</span>
-      <Link
-        to={ROUTE_PATH.myPage.edit}
-        className="flex w-[6.25rem] h-7 border border-white rounded-[1.25rem] items-center"
-      >
-        <span className="px-3 text-xs text-white">정보수정</span>
-        <img src="/assets/images/edit-icon.svg" alt="editIcon" />
-      </Link>
+      <MyPageEditButton />
     </section>
   );
 };
