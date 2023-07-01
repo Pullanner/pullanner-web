31e7246afd1cfe53a2216ace0c939f7478246d14
soralee <sora2821@gmail.com>
2023-07-01 21:08:49 +0900

    [#26] Refactor: add image prefix to each parameter name of ProfileImage

--- src/components/ProfileImage/index.tsx
+++ src/components/ProfileImage/index.tsx
@@ -1,15 +1,15 @@
 type ProfileImageProps = {
-  url: string;
-  size: string;
+  imageUrl: string;
+  imageSize: string;
 };
 
-export const ProfileImage = ({ url, size }: ProfileImageProps) => {
+export const ProfileImage = ({ imageUrl, imageSize }: ProfileImageProps) => {
   return (
     <img
       className="mb-[1.938rem] rounded-full"
-      src={url}
+      src={imageUrl}
       alt="profileImage"
-      style={{ width: size, height: size }}
+      style={{ width: imageSize, height: imageSize }}
     />
   );
 };
