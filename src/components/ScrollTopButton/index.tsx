520b15118fcb80e1ba4fe42d7a18f565f1e1095f
Jayun <jay.lee.2280@gmail.com>
2023-07-03 12:29:47 +0900

    [#37] Feat: implement and apply scroll top button component

---
+++ src/components/ScrollTopButton/index.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+
+export const ScrollTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const outletContainer = document.querySelector('#outlet-container') as HTMLElement;
+
+  const handleScrollToTop = () => {
+    outletContainer.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    const showScrollTopButton = () => {
+      if (outletContainer.scrollTop > 0) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+    outletContainer.addEventListener('scroll', showScrollTopButton);
+    return () => {
+      outletContainer.removeEventListener('scroll', showScrollTopButton);
+    };
+  }, []);
+
+  return (
+    <button
+      type="button"
+      className="bg-white sticky float-right bottom-3 right-3 w-10 h-10 rounded-full opacity-80"
+      onClick={handleScrollToTop}
+      style={{ display: `${isVisible ? 'block' : 'none'}` }}
+    >
+      <img src="/assets/images/up-arrow.png" alt="Top" />
+    </button>
+  );
+};
