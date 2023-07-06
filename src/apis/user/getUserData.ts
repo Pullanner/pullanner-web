30715682167ec3704b586c0016d284a90bf02e8d
soralee <sora2821@gmail.com>
2023-07-06 23:02:48 +0900

    [#21] Refactor: separate getUserData functions to apis/user

---
+++ src/apis/user/getUserData.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+// import { Dispatch, SetStateAction } from 'react';
+
+import { API_PATH } from '@/constants/apiPath';
+// import { getAuthRequest } from '@/lib/axios/useAuthApi';
+
+export const getUserDataTest = async () => {
+  try {
+    const response = await axios.get(API_PATH.user);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// 실제 API 요청에 대한 useQuery
+// export const useUserData = (
+//   accessToken: string,
+//   setAccessToken: Dispatch<SetStateAction<string>>,
+// ) => {
+//   return useQuery([queryKeys.userData, accessToken, setAccessToken], () => {
+//     return getAuthRequest(API_PATH.user, accessToken, setAccessToken);
+//   });
+// };
--- src/lib/react-query/useUserData.ts
+++ src/lib/react-query/useUserData.ts
@@ -1,20 +1,10 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
-// import { Dispatch, SetStateAction } from 'react';
 
+import { getUserDataTest } from '@/apis/user/getUserData';
 import { API_PATH } from '@/constants/apiPath';
-// import { getAuthRequest } from '@/lib/axios/useAuthApi';
 
 import { queryKeys } from './queryKeys';
-
-const getUserDataTest = async () => {
-  try {
-    const response = await axios.get(API_PATH.user);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
-};
 
 const postUserDataTest = async (nickname: string) => {
   try {
@@ -41,13 +31,3 @@ export const useMutateNickname = () => {
     },
   });
 };
-
-// 실제 API 요청에 대한 useQuery
-// export const useUserData = (
-//   accessToken: string,
-//   setAccessToken: Dispatch<SetStateAction<string>>,
-// ) => {
-//   return useQuery([queryKeys.userData, accessToken, setAccessToken], () => {
-//     return getAuthRequest(API_PATH.user, accessToken, setAccessToken);
-//   });
-// };
