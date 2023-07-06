71ebd22cf0b61d393110097378443904616be734
soralee <sora2821@gmail.com>
2023-07-06 22:36:48 +0900

    [#21] Feat: add form component to check nickname duplication
    - add handler function to update inputValue and disable save button when typing
    - add handler function to check nickname duplication by calling validatino function
    - separate relative constants to constants file

---
+++ src/components/DuplicationCheckForm/constants.ts
@@ -0,0 +1,10 @@
+export const VALID_INPUT_STATUS = 'valid';
+export const VALID_INPUT_DESCRIPTION = '사용 가능한 닉네임입니다.';
+export const VALID_INPUT_DESCRIPTION_STYLE = 'text-[#63B0FF]';
+export const INVALID_INPUT_STATUS = 'invalid';
+export const INVALID_INPUT_DESCRIPTION = '이미 존재하는 닉네임입니다.';
+export const INVALID_INPUT_DESCRIPTION_STYLE = 'text-[#FF5E62]';
+export const DUPLICATION_CHECK_BUTTON_STYLE = {
+  active: 'text-primary border border-primary',
+  inactive: 'text-gray-500 border border-gray-500',
+} as const;
---
+++ src/components/DuplicationCheckForm/index.tsx
@@ -0,0 +1,94 @@
+import { useState, Dispatch, SetStateAction, ChangeEvent } from 'react';
+
+import {
+  VALID_INPUT_STATUS,
+  INVALID_INPUT_STATUS,
+  DUPLICATION_CHECK_BUTTON_STYLE,
+} from './constants';
+import { InputStatusType, InputDescription } from './InputDescription';
+
+type AsyncValidateFunction<T> = (value: T) => Promise<boolean>;
+
+type DuplicationCheckFormProps = {
+  inputName: string;
+  defaultValue?: string;
+  minLength: number;
+  maxLength: number;
+  setValidInputValue: Dispatch<SetStateAction<string>>;
+  validationFunction: AsyncValidateFunction<string>;
+};
+
+export const DuplicationCheckForm = ({
+  inputName,
+  defaultValue = '',
+  minLength,
+  maxLength,
+  setValidInputValue,
+  validationFunction,
+}: DuplicationCheckFormProps) => {
+  const [inputValue, setInputValue] = useState(defaultValue);
+  const [inputStatus, setInputStatus] = useState<InputStatusType>(INVALID_INPUT_STATUS);
+  const [showValidationResult, setValidationResultShowed] = useState(false);
+  const DuplicationCheckButtonState = inputValue.length >= minLength ? 'active' : 'inactive';
+  let isFirstTyping = true;
+
+  const handleInputChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+    if (isFirstTyping) {
+      setValidInputValue('');
+      isFirstTyping = false;
+    }
+    setInputValue(value);
+    setValidationResultShowed(false);
+  };
+
+  const handleDuplicationCheckButtonClick = async () => {
+    try {
+      const isInputNotDuplicated = await validationFunction(inputValue);
+      if (isInputNotDuplicated) {
+        setInputStatus(VALID_INPUT_STATUS);
+        setValidInputValue(inputValue);
+      } else {
+        setInputStatus(INVALID_INPUT_STATUS);
+        setValidInputValue('');
+      }
+      setValidationResultShowed(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form className="flex flex-col">
+      <label className="inline-block mb-2.5 text-sm" htmlFor={inputName}>
+        {inputName}
+      </label>
+      <div className="flex h-11 justify-between items-center px-3 rounded-[0.313rem] bg-[#161616]">
+        <input
+          id={inputName}
+          name={inputName}
+          type="text"
+          className="text-base focus:outline-none bg-transparent focus:bg-transparent"
+          value={inputValue}
+          onChange={handleInputChange}
+          minLength={minLength}
+          maxLength={maxLength}
+          aria-describedby="patternDescription"
+        />
+        <button
+          type="button"
+          className={`w-[4.875rem] h-[1.875rem] rounded-[0.313rem] text-base ${DUPLICATION_CHECK_BUTTON_STYLE[DuplicationCheckButtonState]}`}
+          onClick={handleDuplicationCheckButtonClick}
+        >
+          중복확인
+        </button>
+      </div>
+      <InputDescription
+        inputName={inputName}
+        minLength={minLength}
+        maxLength={maxLength}
+        showValidationResult={showValidationResult}
+        inputStatus={inputStatus}
+      />
+    </form>
+  );
+};
