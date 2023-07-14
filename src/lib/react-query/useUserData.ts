08627ba97db49c648f30d1a3c927d3c6e7d616d1
soralee <sora2821@gmail.com>
2023-07-14 15:21:54 +0900

    [#46] Feat: add useMutateNickname function with accessToken as annotation

--- src/lib/react-query/useUserData.ts
+++ src/lib/react-query/useUserData.ts
@@ -2,7 +2,7 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { getUserDataTest, postUserDataTest } from '@/apis/user';
 // import { API_PATH } from '@/constants';
-// import { getAuthRequest } from '@/lib/axios/useAuthApi';
+// import { getAuthRequest, postAuthRequest } from '@/lib/axios/useAuthApi';
 
 import { queryKeys } from './queryKeys';
 
@@ -32,5 +32,21 @@ export const useMutateNickname = () => {
 // ) => {
 //   return useQuery([queryKeys.userData, accessToken, setAccessToken], () => {
 //     return getAuthRequest(API_PATH.user, accessToken, setAccessToken);
+//   });
+// };
+
+// 실제 API 요청에 대한 useMutateNickname
+// export const useMutateNickname = (
+//   accessToken: string,
+//   setAccessToken: Dispatch<SetStateAction<string>>,
+// ) => {
+//   const queryClient = useQueryClient();
+//   return useMutation({
+//     mutationFn: (nickname: string) => {
+//       return postAuthRequest(API_PATH.user, { nickname }, accessToken, setAccessToken);
+//     },
+//     onSuccess: () => {
+//       return queryClient.invalidateQueries({ queryKey: [queryKeys.userData] });
+//     },
 //   });
 // };
