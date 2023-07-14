2b1b436fc7802d20e91c07848a91fd1390294ac9
soralee <sora2821@gmail.com>
2023-07-14 15:17:12 +0900

    [#46] Feat: add validateNickname function with accessToken as annotation

--- src/apis/user/validateNickname.ts
+++ src/apis/user/validateNickname.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 import { API_PATH } from '@/constants';
+// import { getAuthRequest } from '@/lib/axios/useAuthApi';
+
+// import type { Dispatch, SetStateAction } from 'react';
 
 const SUCCESS_RESPONSE_CODE = 'U04';
 
@@ -15,3 +18,23 @@ export const validateNickname = async (nickname: string) => {
     return false;
   }
 };
+
+// 실제 API 요청 (with Access Token)
+// export const validateNickname = async (
+//   nickname: string,
+//   accessToken: string,
+//   setAccessToken: Dispatch<SetStateAction<string>>,
+// ) => {
+//   try {
+//     const params = new URLSearchParams();
+//     params.append('nickname', nickname);
+//     const data = await getAuthRequest(API_PATH.nicknameValidation, accessToken, setAccessToken, {
+//       params,
+//     });
+//     console.log(data);
+//     return data.code === SUCCESS_RESPONSE_CODE;
+//   } catch (error) {
+//     console.log(error);
+//     return false;
+//   }
+// };
