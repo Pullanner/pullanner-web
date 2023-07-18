741b4e3c39c51ccddca6b0fe89002211f6748e08
Jayun <jay.lee.2280@gmail.com>
2023-07-18 12:01:24 +0900

    Feat: add banner in Summary page

--- src/constants/bannerData.ts
+++ src/constants/bannerData.ts
@@ -10,10 +10,11 @@ export const BANNER_DATA = {
     description: '풀래너에 나의 풀업 기록을 남겨볼까요? 꾸준히 기록하며 귀여운 뱃지도 모아보세요.',
     backgroundImgSrc: '/assets/images/banner/journal.png',
   },
-  dashboard: {
-    title: 'Pullanner Dashboard',
-    description: '나의 풀업 운동 기록들을 시각적으로 한눈에 확인해보세요.',
-    backgroundImgSrc: '/assets/images/banner/dashboard.png',
+  summary: {
+    title: 'Pull-up Summary',
+    description:
+      '자신의 성장을 증명하는 그래프로 풀업 운동 실력을 한 눈에 파악하고, 동기부여를 받아보세요.',
+    backgroundImgSrc: '/assets/images/banner/summary.png',
   },
   community: {
     title: 'Pullanner Community',
--- src/pages/Summary/index.tsx
+++ src/pages/Summary/index.tsx
@@ -1,8 +1,14 @@
+import { Banner } from '@/components/Banner';
+import { BANNER_DATA } from '@/constants';
+
 export const Summary = () => {
   return (
-    <div className="mt-12 flex flex-col items-center p-5 text-lg">
-      <p>Summary 페이지는 현재 준비중입니다.</p>
-      <p>조금만 기다려 주세요!</p>
-    </div>
+    <>
+      <Banner data={BANNER_DATA.summary} />
+      <div className="mt-12 flex flex-col items-center p-5 text-lg">
+        <p>Summary 페이지는 현재 준비중입니다.</p>
+        <p>조금만 기다려 주세요!</p>
+      </div>
+    </>
   );
 };
