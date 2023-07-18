c8238b95aa97fb0655aca998d18b96abf990e6c4
soralee <sora2821@gmail.com>
2023-07-18 19:11:27 +0900

    [#47] Feat: add ModalButton component
    - imageUrl, isFocused is optional parameter
    - when isFocused is true, button is focused by using ref
    - when imageUrl exist, button include img tag

---
+++ src/components/Modal/ModalButton.tsx
@@ -0,0 +1,30 @@
+import { useRef, useLayoutEffect } from 'react';
+
+import type { RefObject } from 'react';
+
+type ModalButtonProps = {
+  text: string;
+  handler: () => void;
+  imageUrl?: string;
+  isFocused?: boolean;
+};
+
+export const ModalButton = ({ text, handler, imageUrl, isFocused }: ModalButtonProps) => {
+  const buttonRef = useRef<HTMLButtonElement>() as RefObject<HTMLButtonElement>;
+  useLayoutEffect(() => {
+    if (isFocused && buttonRef.current) {
+      buttonRef.current.focus();
+    }
+  }, [isFocused]);
+  return (
+    <button
+      type="button"
+      className="flex h-14 w-20 cursor-pointer flex-col items-center justify-center gap-y-0.5 rounded-md bg-[#373737] focus:bg-primary focus:text-black"
+      onClick={handler}
+      ref={buttonRef}
+    >
+      {imageUrl && <img src={imageUrl} alt="emoticon" className="h-5 w-5" />}
+      <span className="inline-block text-sm">{text}</span>
+    </button>
+  );
+};
