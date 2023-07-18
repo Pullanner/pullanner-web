fddff5fb4e4b301ff4daa65b5216e147ade17826
soralee <sora2821@gmail.com>
2023-07-18 19:14:28 +0900

    [#47] Feat: add logic to show modal when withdrawal button clicked

--- src/pages/EditMyPage/index.tsx
+++ src/pages/EditMyPage/index.tsx
@@ -5,6 +5,9 @@ import { validateNickname } from '@/apis/user';
 import { DimmedButton } from '@/components/buttons/DimmedButton';
 import { SaveButton } from '@/components/buttons/SaveButton';
 import { DuplicationCheckForm } from '@/components/DuplicationCheckForm';
+import { Modal } from '@/components/Modal';
+import { ModalButton } from '@/components/Modal/ModalButton';
+import { PromptText } from '@/components/Modal/PromptText';
 import { ROUTE_PATH } from '@/constants';
 import { useMutateNickname } from '@/lib/react-query/useUserData';
 
@@ -18,6 +21,7 @@ export const EditMyPage = () => {
     state: { nickname },
   } = useLocation();
   const [nicknameValue, setNicknameValue] = useState(nickname);
+  const [showModal, setShowModal] = useState(false);
   const { mutate } = useMutateNickname();
   const navigate = useNavigate();
 
@@ -26,6 +30,18 @@ export const EditMyPage = () => {
       mutate(nicknameValue);
       navigate(ROUTE_PATH.myPage.index);
     }
+  };
+
+  const handleWithdrwalButtonClick = () => {
+    setShowModal(true);
+  };
+
+  const handleOkButtonClick = () => {
+    navigate('/withdrawal');
+  };
+
+  const handleCancleButtonClick = () => {
+    setShowModal(false);
   };
 
   return (
@@ -45,7 +61,31 @@ export const EditMyPage = () => {
         />
       </div>
 
-      <DimmedButton name="회원탈퇴" />
+      <DimmedButton name="회원탈퇴" handler={handleWithdrwalButtonClick} />
+      {showModal && (
+        <Modal>
+          <PromptText>
+            <p className="mb-2.5 text-xs">
+              회원 탈퇴시 Pullanner의<span className="font-extrabold">모든 데이터가 삭제</span>
+              됩니다.
+            </p>
+            <p className="text-xs">정말 탈퇴하시겠습니까?</p>
+          </PromptText>
+          <div className="mt-4 flex w-full justify-around">
+            <ModalButton
+              text="네"
+              handler={handleOkButtonClick}
+              imageUrl="/assets/images/emotion/2.svg"
+            />
+            <ModalButton
+              text="아니오"
+              handler={handleCancleButtonClick}
+              imageUrl="/assets/images/emotion/5.svg"
+              isFocused
+            />
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
