8dfe3e3f875134c07e34b725b8c472eee428fa8e
soralee <sora2821@gmail.com>
2023-07-27 01:43:09 +0900

    [#62] Feat: add userDataAtom using atomWithStorage

---
+++ src/stores/atoms/userDataAtom.ts
@@ -0,0 +1,18 @@
+import { atomWithStorage, createJSONStorage } from 'jotai/utils';
+
+const storage = createJSONStorage(() => {
+  return sessionStorage;
+});
+
+export type UserData = {
+  userId: number;
+  name: string;
+  nickname: string;
+  email: string;
+  profileImage: string;
+  oauthProvider: string;
+  level: 1 | 2 | 3 | 4 | 5;
+  journalCount: number;
+};
+
+export const userDataAtom = atomWithStorage('userData', null, storage);
