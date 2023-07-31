e9611507ffbe37af2fa4e55b934c694dc4ca0b2c
Jayun <jay.lee.2280@gmail.com>
2023-07-31 13:59:19 +0900

    [#71] Design: insert pull-up name in roadmap card

--- src/pages/Roadmap/RoadmapCard.tsx
+++ src/pages/Roadmap/RoadmapCard.tsx
@@ -5,13 +5,23 @@ type RoadmapCardProps = {
   id: number;
   imageSrc: string;
   description: string;
+  title: string;
 };
 
-export const RoadmapCard = ({ color, id, imageSrc, description }: RoadmapCardProps) => {
+export const RoadmapCard = ({ color, id, imageSrc, description, title }: RoadmapCardProps) => {
   return (
     <div className="rounded-xl" style={{ backgroundColor: color }}>
-      <Link to={`/roadmap/${id}`}>
+      <Link to={`/roadmap/${id}`} className="relative">
         <img src={imageSrc} alt={description} />
+        <p
+          className="absolute bottom-2 right-2 rounded-full bg-[#ffffffb3] px-1 text-base tracking-widest text-black drop-shadow-lg"
+          style={{
+            fontFamily: 'Annie Use Your Telescope, cursive',
+            textShadow: '0.5px 0.5px 0.5px black',
+          }}
+        >
+          {title}
+        </p>
       </Link>
     </div>
   );
