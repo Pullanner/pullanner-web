09edc7f7b5681ab3bd94b42dcccef9fad50909d9
soralee <sora2821@gmail.com>
2023-08-07 21:31:02 +0900

    [#70] Feat: add WorkoutResult component
    - 선택한 wokout data과 선택하지 않은 workout data를 보여주는 형식이 비슷하므로 WorkoutResult 컴포넌트로 구현하여 사용함
    - props로 받은 workoutData에 map 메서드를 호출하여 WorkoutCard를 반환하도록 함
    - props로 받은 textContents를 사용하여 관련된 title, plan, description을 보여줌

---
+++ src/pages/SetupResult/WorkoutResult.tsx
@@ -0,0 +1,47 @@
+import { WorkoutCard } from '@/pages/SelectWorkout/WorkoutCard';
+
+type WorkoutInfo = {
+  id: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+  imageSrc: string;
+  title: string;
+  difficulty: number;
+  description: string;
+  color: string;
+};
+
+type WorkoutResultProps = {
+  workoutData: WorkoutInfo[];
+  textContents: {
+    title: string;
+    plan: string;
+    description: string;
+  };
+};
+
+export const WorkoutResult = ({ workoutData, textContents }: WorkoutResultProps) => {
+  return (
+    <div>
+      <p className="pb-2 text-base font-bold text-primary">{textContents.title}</p>
+      <p className="pb-5 text-xs">
+        <span className="font-extrabold">{textContents.plan}</span>
+        {textContents.description}
+      </p>
+
+      <div className="flex flex-row flex-wrap justify-center gap-x-3.5 gap-y-5 pb-7">
+        {workoutData.map(({ id, title, imageSrc, color }) => {
+          return (
+            <WorkoutCard
+              key={id}
+              id={id}
+              title={title}
+              imageSrc={imageSrc}
+              color={color}
+              width="4.75rem"
+              height="6.375rem"
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
