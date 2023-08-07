07b0c5e8bdf34f4da5fb184a3efb49a1ca7254be
soralee <sora2821@gmail.com>
2023-08-07 21:06:00 +0900

    [#70] Feat: mock data and GET, POST response as to workout

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,7 @@
 import planHandler from '@/mocks/plans/handler';
 import userHandler from '@/mocks/user/handler';
+import workoutHandler from '@/mocks/workouts/handler';
 
-const handlers = [...userHandler, ...planHandler];
+const handlers = [...userHandler, ...planHandler, ...workoutHandler];
 
 export default handlers;
--- src/mocks/user/data.ts
+++ src/mocks/user/data.ts
@@ -1,7 +1,7 @@
 export const USER_DATA = {
   userId: 1,
   name: '김봉철',
-  nickname: '봉철',
+  nickname: null,
   email: 'bongcheol@gmail.com',
   profileImage:
     'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
