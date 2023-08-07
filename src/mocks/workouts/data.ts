f24124eccf43524852a020bcaee1e3d972891c0c
soralee <sora2821@gmail.com>
2023-08-07 20:57:08 +0900

    [#70] Feat: mock workoutsData and add workouts api path

---
+++ src/mocks/workouts/data.ts
@@ -0,0 +1,10 @@
+export const WORKOUTS_DATA = [
+  { name: 'Hanging', selected: false },
+  { name: 'Jumping Pull-up', selected: false },
+  { name: 'Band Pull-up', selected: false },
+  { name: 'Chin-up', selected: false },
+  { name: 'Pull-up', selected: false },
+  { name: 'Chest to Bar', selected: false },
+  { name: 'Archer Pull-up', selected: false },
+  { name: 'Muscle up', selected: false },
+];
