0fb595969ea1f79f5d5815acc11af54537826852
soralee <sora2821@gmail.com>
2023-08-15 13:51:15 +0900

    [#90] Refactor: show modal when clicking logout button

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,7 +1,11 @@
 import { useSetAtom, useAtomValue } from 'jotai';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { DimmedButton } from '@/components/buttons/DimmedButton';
+import { Modal } from '@/components/Modal';
+import { ModalButton } from '@/components/Modal/ModalButton';
+import { SubText } from '@/components/Modal/ModalText';
 import { API_PATH, ROUTE_PATH } from '@/constants';
 import { axiosInstance } from '@/lib/axios/instance';
 import { accessTokenAtom } from '@/stores/atoms/accessTokenAtom';
@@ -12,19 +16,30 @@ import { ProgressSection } from './ProgressSection';
 import { TabSection } from './TabSection';
 import { UserSection } from './UserSection';
 
+const MODAL_TEXT = '정말 로그아웃하시겠습니까?';
+
 export const MyPage = () => {
   const setAccessToken = useSetAtom(accessTokenAtom);
   const setLoginState = useSetAtom(loginStateAtom);
   const userData = useAtomValue(userDataAtom) as UserData;
   const setUserData = useSetAtom(userDataAtom);
   const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
 
   const handleLogoutButtonClick = () => {
+    setShowModal(true);
+  };
+
+  const handleOkButtonClick = () => {
     axiosInstance.delete(API_PATH.token);
     setLoginState(false);
     setAccessToken('');
     setUserData(null);
     navigate(ROUTE_PATH.root);
+  };
+
+  const handleCancleButtonClick = () => {
+    setShowModal(false);
   };
 
   // TODO: 추후에 로딩페이지로 교체하기
@@ -42,6 +57,15 @@ export const MyPage = () => {
       <div className="flex justify-center">
         <DimmedButton name="로그아웃" handler={handleLogoutButtonClick} />
       </div>
+      {showModal && (
+        <Modal>
+          <SubText textStyle="p-5">{MODAL_TEXT}</SubText>
+          <div className="mb-5 flex w-full justify-around">
+            <ModalButton text="네" handler={handleOkButtonClick} />
+            <ModalButton text="아니오" handler={handleCancleButtonClick} isPrimary />
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
