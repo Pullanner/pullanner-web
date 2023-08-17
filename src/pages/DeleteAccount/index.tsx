5d7a7f04752e54a7e8ecf5e663c8f202c78dcc17
soralee <sora2821@gmail.com>
2023-08-17 22:32:41 +0900

    [#61] Feat: add DeleteAccount component
    - '인증 코드 전송하기'버튼의 onClick 핸들러에서 isTimerActive를 true로 변경하고 AuthenticationCodeInput에 focus되도록 함

---
+++ src/pages/DeleteAccount/index.tsx
@@ -0,0 +1,39 @@
+import { useState, useRef } from 'react';
+
+import { SaveButton } from '@/components/buttons/SaveButton';
+
+import { AuthenticationCodeInput } from './AuthenticationCodeInput';
+import { DeleteAccountDescription } from './DeleteAccountDescription';
+
+export const DeleteAccount = () => {
+  const [isTimerActive, setTimerActive] = useState(false);
+  const [isSendCodeButtonActive, setSendCodeButtonActive] = useState(true);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSendCodeButtonClick = () => {
+    setTimerActive(true);
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
+  return (
+    <div className="w-full px-5">
+      <h1 className="pb-3.5 pt-5 text-xl font-extrabold">회원 탈퇴</h1>
+      <DeleteAccountDescription />
+      <SaveButton
+        isActive={isSendCodeButtonActive}
+        handleButtonClick={handleSendCodeButtonClick}
+        width="100%"
+        height="2.75rem"
+        text="인증 코드 전송하기"
+        className="text-sm"
+      />
+      <AuthenticationCodeInput
+        isTimerActive={isTimerActive}
+        setSendCodeButtonActive={setSendCodeButtonActive}
+        ref={inputRef}
+      />
+    </div>
+  );
+};
