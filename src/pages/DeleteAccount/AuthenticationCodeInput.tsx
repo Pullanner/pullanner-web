215ceec923f3284ab14ae25e8f27d906de148c70
soralee <sora2821@gmail.com>
2023-08-18 19:48:29 +0900

    [#61] Feat: add logic as to deleteAccountWithAuthenticationCode func
    - '인증코드 전송하기' 버튼을 클릭할 경우, deleteAccountWithAuthenticationCode 함수를 호출함
    - deleteAccountWithAuthenticationCode 함수의 반환값이 false인 경우, isDeleteRequestFailed가 true가 되고 '유효하지 않은 인증번호입니다' 문구를 보여주고  '인증코드 전송하기' 버튼이 활성화됨
    - deleteAccountWithAuthenticationCode 함수의 반환값이 true인 경우, showModal이 true가 되고 '회원탈퇴가 왼료되었습니다' 모달을 보여줌
    - 회원탈퇴 관련 모달의 '확인' 버튼을 클릭하면 Roadmap 페이지로 이동함

--- src/pages/DeleteAccount/AuthenticationCodeInput.tsx
+++ src/pages/DeleteAccount/AuthenticationCodeInput.tsx
@@ -1,10 +1,30 @@
+import { useAtom } from 'jotai';
 import { useState, forwardRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { deleteAccountWithAuthenticationCode } from '@/apis/user/deleteAccountWithAuthenticationCode';
+import { Modal } from '@/components/Modal';
+import { ModalButton } from '@/components/Modal/ModalButton';
+import { MainText, SubText, ModalText } from '@/components/Modal/ModalText';
+import { ROUTE_PATH } from '@/constants';
+import { accessTokenAtom } from '@/stores/atoms/accessTokenAtom';
 
 import { Timer } from './Timer';
 
 import type { ChangeEvent, Dispatch, SetStateAction, ForwardedRef } from 'react';
 
 const MAX_INPUT_LENGTH = 6;
+
+const TEXT_CONTENTS = {
+  invalidAuthenticationCodeDescription: '유효하지 않은 인증번호입니다.',
+  modal: {
+    main: '회원탈퇴',
+    sub: [
+      'pullanner 회원탈퇴가 완료되었습니다.',
+      '그 동안 Pullanner 서비스를 이용해주셔서 감사합니다.',
+    ],
+  },
+} as const;
 
 const BUTTON_STATE = {
   active: 'active',
@@ -30,25 +50,48 @@ export const AuthenticationCodeInput = forwardRef<HTMLInputElement, Authenticati
   ) => {
     const [authenticationCode, setAuthenticationCode] = useState('');
     const [deleteAccountButtonActive, setDeleteAccountButtonActive] = useState(false);
+    const [isDeleteRequestFailed, setShowInvalidAuthenticationCodeDescription] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [accessToken, setAccessToken] = useAtom(accessTokenAtom);
+    const navigate = useNavigate();
 
     const deleteAccountButtonState = deleteAccountButtonActive
       ? BUTTON_STATE.active
       : BUTTON_STATE.inactive;
 
-    const handleDeleteAccountButtonClick = () => {
+    const handleDeleteAccountButtonClick = async () => {
       if (!deleteAccountButtonActive) {
         return;
       }
-      console.log(authenticationCode);
+
+      const isAuthenticationCodeVaild = await deleteAccountWithAuthenticationCode(
+        authenticationCode,
+        accessToken,
+        setAccessToken,
+      );
+
+      if (isAuthenticationCodeVaild) {
+        setShowModal(true);
+      } else {
+        setShowInvalidAuthenticationCodeDescription(true);
+        setSendCodeButtonActive(true);
+      }
     };
 
     const handleInputChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
       const inputNumberValue = value.replace(/[^0-9]/g, '');
       setAuthenticationCode(inputNumberValue);
       setSendCodeButtonActive(false);
+      setShowInvalidAuthenticationCodeDescription(false);
+
       if (inputNumberValue.length === MAX_INPUT_LENGTH) {
         setDeleteAccountButtonActive(true);
       }
+    };
+
+    const handleOkButtonClick = () => {
+      setShowModal(false);
+      navigate(ROUTE_PATH.roadmap.index);
     };
 
     return (
@@ -75,6 +118,31 @@ export const AuthenticationCodeInput = forwardRef<HTMLInputElement, Authenticati
             회원탈퇴
           </button>
         </div>
+        {isDeleteRequestFailed && (
+          <p id="valdationResult" className="pt-3 text-sm text-[#FF5E62]">
+            {TEXT_CONTENTS.invalidAuthenticationCodeDescription}
+          </p>
+        )}
+        {showModal && (
+          <Modal>
+            <ModalText textStyle="flex flex-col items-center justify-center px-5 pt-5">
+              <MainText textStyle="pb-2.5">{TEXT_CONTENTS.modal.main}</MainText>
+              <div className="border-t-2 border-[#686868] pt-2.5 text-center">
+                {TEXT_CONTENTS.modal.sub.map((text) => {
+                  return <SubText key={text}>{text}</SubText>;
+                })}
+              </div>
+            </ModalText>
+            <div className="mb-5 flex w-full justify-center pt-2.5">
+              <ModalButton
+                text="확인"
+                buttonStyle="w-[5.625rem] h-7"
+                handler={handleOkButtonClick}
+                isPrimary
+              />
+            </div>
+          </Modal>
+        )}
       </div>
     );
   },
