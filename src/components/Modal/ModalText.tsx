fe83470a713e932bb1e51be41b097d62dea70fd8
soralee <sora2821@gmail.com>
2023-08-18 19:55:03 +0900

    [#47] Design: modify design as to Modal, ModalText
    - Modal에 고정 width값을 추가함
    - ModalText에서 max-width 제거
    - MainText에서 magin-bottom 제거

--- src/components/Modal/ModalText.tsx
+++ src/components/Modal/ModalText.tsx
@@ -6,11 +6,11 @@ type TextProps = {
 };
 
 export const ModalText = ({ children, textStyle = '' }: TextProps) => {
-  return <div className={`${textStyle} max-w-[20rem]`}>{children}</div>;
+  return <div className={`${textStyle}`}>{children}</div>;
 };
 
 export const MainText = ({ children, textStyle }: TextProps) => {
-  return <p className={`${textStyle} mb-4 text-base font-extrabold`}>{children}</p>;
+  return <p className={`${textStyle} text-base font-extrabold`}>{children}</p>;
 };
 
 export const SubText = ({ children, textStyle }: TextProps) => {
