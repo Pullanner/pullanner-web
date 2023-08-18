48b26e8b11925cb89f72c4b8a731fada820cf17e
soralee <sora2821@gmail.com>
2023-08-18 11:44:32 +0900

    [#61] Feat: add SuccessIcon to components/icons folder

---
+++ src/components/icons/SuccessIcon/index.tsx
@@ -0,0 +1,9 @@
+export const SuccessIcon = () => {
+  return (
+    <img
+      className="mr-2 inline-block h-6 w-6"
+      src="/assets/images/success_icon.svg"
+      alt="successIcon"
+    />
+  );
+};
