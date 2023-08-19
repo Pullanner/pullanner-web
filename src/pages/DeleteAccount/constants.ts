85fa2f33549c4b82f0f8c7140ac33fb41450332b
soralee <sora2821@gmail.com>
2023-08-20 01:34:22 +0900

    [#61] Refactor: correct the contents of TEXT_CONTENTS for grammer
    - '회원탈퇴'를 맞춤법에 맞게 '회원 탈퇴'로 변경

--- src/pages/DeleteAccount/constants.ts
+++ src/pages/DeleteAccount/constants.ts
@@ -3,9 +3,9 @@ export const MAX_INPUT_LENGTH = 6;
 export const TEXT_CONTENTS = {
   invalidAuthenticationCodeDescription: '유효하지 않은 인증번호입니다.',
   modal: {
-    main: '회원탈퇴',
+    main: '회원 탈퇴',
     sub: [
-      'pullanner 회원탈퇴가 완료되었습니다.',
+      'pullanner 회원 탈퇴가 완료되었습니다.',
       '그동안 Pullanner 서비스를 이용해주셔서 감사합니다.',
     ],
   },
