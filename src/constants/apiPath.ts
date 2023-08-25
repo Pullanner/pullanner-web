598be76c1280eabb29246d78dab48816458e805f
soralee <sora2821@gmail.com>
2023-08-25 13:28:57 +0900

    [#99] Refactor: put together keys related to user in API_APTH

--- src/constants/apiPath.ts
+++ src/constants/apiPath.ts
@@ -2,9 +2,9 @@ export const API_PATH = {
   user: '/api/users',
   userNicknameValidation: '/api/users/duplicate',
   userEmail: '/api/users/mail',
+  userWorkouts: '/api/users/workouts',
   token: '/api/tokens',
   plans: '/api/plans',
-  userWorkouts: '/api/users/workouts',
 } as const;
 
 export type ApiPathType = (typeof API_PATH)[keyof typeof API_PATH];
