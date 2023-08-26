f5ef6f5edb2db25053ceb7c50d5727ae03ffc01a
Jayun <jay.lee.2280@gmail.com>
2023-08-26 14:12:41 +0900

    [#94] Feat: add redirection to login page when Plan button is clicked without being logged in

--- src/pages/Plan/PlanButtons/index.tsx
+++ src/pages/Plan/PlanButtons/index.tsx
@@ -2,22 +2,25 @@ import { useAtomValue } from 'jotai';
 import { Link } from 'react-router-dom';
 
 import { ROUTE_PATH } from '@/constants';
+import { loginStateAtom } from '@/stores/atoms/loginStateAtom';
 import { selectedDateAtom } from '@/stores/atoms/selectedDateAtom';
 
 export const PlanButtons = () => {
   const selectedDate = useAtomValue(selectedDateAtom);
+  const isLoggedIn = useAtomValue(loginStateAtom);
+  const planLink = isLoggedIn ? ROUTE_PATH.plan.new : ROUTE_PATH.login;
 
   return (
     <div className="mb-3 flex justify-around p-3">
       <Link
-        to={ROUTE_PATH.plan.new}
+        to={planLink}
         state={{ planType: 'strength', date: selectedDate }}
         className="flex items-center rounded-md bg-primary p-3 text-base text-black"
       >
         <span>💪 근력 키우기 플랜</span>
       </Link>
       <Link
-        to={ROUTE_PATH.plan.new}
+        to={planLink}
         state={{ planType: 'master', date: selectedDate }}
         className="flex items-center rounded-md border-2 border-primary bg-zinc-800 p-3 text-base text-white"
       >
