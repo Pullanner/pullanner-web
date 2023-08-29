3980fa73dfb59e782148f95c648f5e60e337af89
Jayun <jay.lee.2280@gmail.com>
2023-08-29 22:16:33 +0900

    [#94] Feat: implement convertToUTCDate function for plan time

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -6,3 +6,12 @@ export const checkPastDate = (dateString: string): boolean => {
 
   return inputDate.isBefore(today, 'day');
 };
+
+export const convertToUTCDate = (dateString: string, timeString: string) => {
+  const [year, month, day] = dateString.split('-').map(Number);
+  const [hour, minute] = timeString.split(':').map(Number);
+
+  const utcDate = new Date(Date.UTC(year, month - 1, day, hour, minute));
+
+  return utcDate.toISOString();
+};
