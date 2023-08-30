c5ccaa6bda3b8f9e43470894b8168233030ed567
soralee <sora2821@gmail.com>
2023-08-30 16:54:12 +0900

    [#101] Refactor: remove error handling logic from deleteAuthRequest

--- src/lib/axios/useAuthApi.ts
+++ src/lib/axios/useAuthApi.ts
@@ -2,9 +2,6 @@ import axios, { RawAxiosRequestHeaders, AxiosHeaders } from 'axios';
 
 import { ApiPathType } from '@/constants';
 import { axiosInstance } from '@/lib/axios/instance';
-import { reissueAccessToken } from '@/utils/reissueAccessToken';
-
-import type { Dispatch, SetStateAction } from 'react';
 
 type Options = {
   headers?: RawAxiosRequestHeaders | AxiosHeaders;
@@ -53,29 +50,20 @@ export const postAuthRequest = async <T>(apiPath: string, payload: T, accessToke
 export const deleteAuthRequest = async (
   apiPath: ApiPathType,
   accessToken: string,
-  setAccessToken: Dispatch<SetStateAction<string>>,
   options?: Options,
 ) => {
-  try {
-    if (isDevMode) {
-      const { data } = await axios.delete(apiPath, { ...options });
-
-      return data;
-    }
-
-    const { data } = await axiosInstance.delete(apiPath, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      ...options,
-    });
+  if (isDevMode) {
+    const { data } = await axios.delete(apiPath, { ...options });
 
     return data;
-  } catch (error) {
-    if (axios.isAxiosError(error) && error.code === '403') {
-      const newAccessToken = await reissueAccessToken();
-      setAccessToken(newAccessToken);
-    }
-    throw error;
   }
+
+  const { data } = await axiosInstance.delete(apiPath, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    ...options,
+  });
+
+  return data;
 };
