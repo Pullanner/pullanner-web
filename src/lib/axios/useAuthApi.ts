c7adda45313517715eccbd867f65540b4d75f481
soralee <sora2821@gmail.com>
2023-08-30 16:11:23 +0900

    [#101] Refactor: remove error handling logic from getAuthRequest

--- src/lib/axios/useAuthApi.ts
+++ src/lib/axios/useAuthApi.ts
@@ -16,30 +16,22 @@ const isDevMode = import.meta.env.DEV;
 export const getAuthRequest = async (
   apiPath: ApiPathType,
   accessToken: string,
-  setAccessToken: Dispatch<SetStateAction<string>>,
   options?: Options,
 ) => {
-  try {
-    if (isDevMode) {
-      const { data } = await axios.get(apiPath, { ...options });
-
-      return data;
-    }
-
-    const { data } = await axiosInstance.get(apiPath, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      ...options,
-    });
+  if (isDevMode) {
+    const { data } = await axios.get(apiPath, { ...options });
 
     return data;
-  } catch (error) {
-    if (axios.isAxiosError(error) && error.code === '403') {
-      const newAccessToken = await reissueAccessToken();
-      setAccessToken(newAccessToken);
-    }
   }
+
+  const { data } = await axiosInstance.get(apiPath, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    ...options,
+  });
+
+  return data;
 };
 
 export const postAuthRequest = async <T>(
