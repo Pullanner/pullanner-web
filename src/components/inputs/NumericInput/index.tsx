e33a553d6d40a64d6a33393205568db57d6d20f2
Jayun <jay.lee.2280@gmail.com>
2023-09-02 14:51:33 +0900

    [#94] Feat: implement NumericInput component

---
+++ src/components/inputs/NumericInput/index.tsx
@@ -0,0 +1,33 @@
+import { ChangeEvent } from 'react';
+
+type NumericInputProps = {
+  inputName: string;
+  value: string;
+  onChange: (value: string) => void;
+  maxLength: number;
+};
+
+export const NumericInput = ({ inputName, onChange, value, maxLength }: NumericInputProps) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value: inputValue } = e.target;
+    const regex = /^[0-9]+$/;
+    if (inputValue === '' || (regex.test(inputValue) && inputValue.length <= maxLength)) {
+      onChange(inputValue);
+    }
+  };
+
+  const handleBlur = () => {
+    onChange(value.replace(/0*(\d+)/, '$1'));
+  };
+
+  return (
+    <input
+      name={inputName}
+      value={value}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      className="w-10 rounded-md bg-[#2e2e2e] p-1 text-center"
+      required
+    />
+  );
+};
