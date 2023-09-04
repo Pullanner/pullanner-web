1cb250a9ca88897a97892017e2a52970837dfce6
Jayun <jay.lee.2280@gmail.com>
2023-09-04 15:40:36 +0900

    [#94] Refactor: modify plan and workout types

--- src/types/plan.ts
+++ src/types/plan.ts
@@ -1,28 +1,36 @@
 export type PullUpSteps = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
 export type Workout = {
+  count: number;
+  set: number;
+  done: boolean;
+  step: PullUpSteps;
+};
+
+export type PlanType = 'strength' | 'master';
+
+export type Plan = {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  planDateTime: string;
+  planName: string;
+  planType: PlanType;
+  workout: Workout[];
+  progress: number;
+  note: string;
+  mainWorkoutStep: PullUpSteps;
+};
+
+export type PlanData = {
+  [date: string]: Plan[];
+};
+
+export type SelectedWorkoutType = {
   id: PullUpSteps;
   name: string;
   color: string;
   count: number;
   set: number;
   total: number;
-};
-
-export type Plan = {
-  id: number;
-  createdAt: string;
-  updatedAt: string;
-  planDate: string;
-  planName: string;
-  planTime: string;
-  planType: string;
-  workout: Workout[];
-  progress: number;
-  note: string;
-  mainColor: string;
-};
-
-export type PlanData = {
-  [date: string]: Plan[];
 };
