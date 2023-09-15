1200c383db00e4cc2b29a99b506aa0db6372f1bb
soralee <sora2821@gmail.com>
2023-09-15 23:57:05 +0900

    [#104] Refactor: change MAX_JOURNAL_COUNT_BY_LEVEL to MAX_EXPERIENCE_POINT_BY_LEVEL
    - 레벨 정책에 따라 레벨별 최대 경험치 값을 수정함

--- src/pages/MyPage/ProgressSection.tsx
+++ src/pages/MyPage/ProgressSection.tsx
@@ -1,20 +1,20 @@
 import { ProgressBar } from '@/components/ProgressBar';
-import { MAX_JOURNAL_COUNT_BY_LEVEL } from '@/constants';
+import { MAX_EXPERIENCE_POINT_BY_LEVEL } from '@/constants';
 import { getPercent } from '@/utils/percent';
 
 type ProgressSectionProps = {
   experiencePoint: number;
-  level: keyof typeof MAX_JOURNAL_COUNT_BY_LEVEL;
+  level: keyof typeof MAX_EXPERIENCE_POINT_BY_LEVEL;
 };
 
 export const ProgressSection = ({ experiencePoint, level }: ProgressSectionProps) => {
-  const percent = getPercent(experiencePoint, MAX_JOURNAL_COUNT_BY_LEVEL[level]);
+  const percent = getPercent(experiencePoint, MAX_EXPERIENCE_POINT_BY_LEVEL[level]);
 
   return (
     <section className="w-full px-5 pb-10">
       <div className="flex justify-between pb-2">
         <span className="text-sm font-semibold text-primary">{`LEVEL ${level}`}</span>
-        <span className="text-xs">{`${experiencePoint} / ${MAX_JOURNAL_COUNT_BY_LEVEL[level]} Exp`}</span>
+        <span className="text-xs">{`${experiencePoint} / ${MAX_EXPERIENCE_POINT_BY_LEVEL[level]} Exp`}</span>
       </div>
       <ProgressBar percent={percent} />
     </section>
