532dc35f04c12f28a7885e6af9f99925cb9d4d16
soralee <sora2821@gmail.com>
2023-09-22 20:26:49 +0900

    [#108] Feat: add UploadProfileImageModal component
    - ProfileImageSection의 input file에서 이미지를 선택했을 때 모달을 통해 선택한 이미지를 미리보기로 보여줌
    - "예" 버튼을 클릭하면 userData의 profileImage가 수정되고 모달창이 닫힘
    - "아니오" 버튼을 클릭하면 모달창이 닫힘
    - modalTypeAtom에 'uploadProfileImage' 타입을 추가함
    - Root 페이지에서 modalType이 'uploadProfileImage'일 때 UploadProfileImage 모달을 렌더링함

---
+++ src/components/modals/UploadProfileImageModal/index.tsx
@@ -0,0 +1,50 @@
+import { useAtomValue, useSetAtom } from 'jotai';
+import { useMemo } from 'react';
+
+import { Modal } from '@/components/modals/Modal';
+import { ModalButton } from '@/components/modals/Modal/ModalButton';
+import { MainText, ModalText } from '@/components/modals/Modal/ModalText';
+import { ProfileImage } from '@/components/ProfileImage';
+import { modalTypeAtom } from '@/stores/atoms/modalTypeAtom';
+import { profileImageDataAtom } from '@/stores/atoms/profileImageDataAtom';
+import { userDataAtom, UserData } from '@/stores/atoms/userDataAtom';
+
+const MODAL_TEXT = '이 프로필 이미지로 변경하시겠습니까?';
+
+export const UploadProfileImageModal = () => {
+  const profileImageData = useAtomValue(profileImageDataAtom);
+  const userData = useAtomValue(userDataAtom) as UserData;
+  const setUserData = useSetAtom(userDataAtom);
+  const setModalType = useSetAtom(modalTypeAtom);
+  const profileImage = useMemo(() => {
+    const imageFile = profileImageData.get('profileImage') as File;
+    const imageUrl = URL.createObjectURL(imageFile);
+
+    return imageUrl;
+  }, [profileImageData]);
+
+  const handleOkButtonClick = () => {
+    setUserData({ ...userData, profileImage });
+    setModalType(null);
+  };
+
+  const handleCancelButtonClick = () => {
+    setModalType(null);
+  };
+
+  return (
+    <Modal>
+      <ModalText textStyle="py-7">
+        <MainText>{MODAL_TEXT}</MainText>
+      </ModalText>
+      <div className="flex justify-center pb-7">
+        <ProfileImage imageUrl={profileImage} imageSize="9rem" />
+      </div>
+
+      <div className="flex w-full">
+        <ModalButton text="네" handler={handleOkButtonClick} />
+        <ModalButton text="아니오" handler={handleCancelButtonClick} isPrimary />
+      </div>
+    </Modal>
+  );
+};
