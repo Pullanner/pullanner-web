e5f687399d637cc72e76c93c5f7b3101fd35e7ec
soralee <sora2821@gmail.com>
2023-10-23 16:57:08 +0900

    [#114] Feat: add MonthDropdown component
    - Antd 라이브러리의 Dropdown을 사용하여 MonthlyTotalCountByEachWorkoutChart 그래프를 월별로 보여주기 위해 필요한 드롭다운을 구현함
    - 드롭다운의 메뉴 아이템을 클릭하면 부모 컴포넌트 MonthlyTotalCountByEachWorkoutChart의 workoutName 상태가 변경됨

---
+++ src/pages/Summary/MonthlyTotalCountByEachWorkoutChart/MonthDropdown.tsx
@@ -0,0 +1,86 @@
+import { Dropdown, type MenuProps } from 'antd';
+import { type MouseEvent } from 'react';
+
+type MonthDropdownProps = {
+  handleDropdownItemClick: ({ currentTarget }: MouseEvent<HTMLButtonElement>) => void;
+  workoutName: string;
+};
+
+export const MonthDropdown = ({ handleDropdownItemClick, workoutName }: MonthDropdownProps) => {
+  const items: MenuProps['items'] = [
+    {
+      key: '1',
+      label: (
+        <button type="button" name="Hanging" onClick={handleDropdownItemClick}>
+          Hanging
+        </button>
+      ),
+    },
+    {
+      key: '2',
+      label: (
+        <button type="button" name="Jumping Pull-Up" onClick={handleDropdownItemClick}>
+          Jumping Pull-Up
+        </button>
+      ),
+    },
+    {
+      key: '3',
+      label: (
+        <button type="button" name="Band Pull-Up" onClick={handleDropdownItemClick}>
+          Band Pull-Up
+        </button>
+      ),
+    },
+    {
+      key: '4',
+      label: (
+        <button type="button" name="Chin-Up" onClick={handleDropdownItemClick}>
+          Chin-Up
+        </button>
+      ),
+    },
+    {
+      key: '5',
+      label: (
+        <button type="button" name="Pull-Up" onClick={handleDropdownItemClick}>
+          Pull-Up
+        </button>
+      ),
+    },
+    {
+      key: '6',
+      label: (
+        <button type="button" name="Chest to Bar" onClick={handleDropdownItemClick}>
+          Chest to Bar
+        </button>
+      ),
+    },
+    {
+      key: '7',
+      label: (
+        <button type="button" name="Archer Pull-Up" onClick={handleDropdownItemClick}>
+          Archer Pull-Up
+        </button>
+      ),
+    },
+    {
+      key: '8',
+      label: (
+        <button type="button" name="Muscle Up" onClick={handleDropdownItemClick}>
+          Muscle Up
+        </button>
+      ),
+    },
+  ];
+
+  return (
+    <div className="pb-3 pl-3">
+      <Dropdown menu={{ items, selectable: true }} placement="bottomLeft" trigger={['click']}>
+        <div className="inline-block rounded-md bg-gray-4 px-2 py-1 text-sm hover:bg-gray-3">
+          {workoutName}
+        </div>
+      </Dropdown>
+    </div>
+  );
+};
