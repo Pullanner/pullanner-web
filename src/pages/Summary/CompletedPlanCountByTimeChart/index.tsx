d4716e504e083bfe71709af7caab5ddf4f38e64f
soralee <sora2821@gmail.com>
2023-10-30 16:26:07 +0900

    [#114] Refactor: use api func to get completedPlanCountData instead using constant
    - completedPlanCountData의 map 메서드 내 콜백함수의 param 이름이 길고 data가 중복되므로 data 접두사를 지움

--- src/pages/Summary/CompletedPlanCountByTimeChart/index.tsx
+++ src/pages/Summary/CompletedPlanCountByTimeChart/index.tsx
@@ -1,34 +1,32 @@
+import { useAtom, useSetAtom } from 'jotai';
 import { Bar, BarChart, Legend, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
 
-const BAR_CHART_DATA = [
-  {
-    time: 'morning',
-    thisMonth: 40,
-    prevMonth: 24,
-  },
-  {
-    time: 'afternoon',
-    thisMonth: 30,
-    prevMonth: 13,
-  },
-  {
-    time: 'evening',
-    thisMonth: 20,
-    prevMonth: 98,
-  },
-];
-
-const MOST_WORKOUT_TIME = BAR_CHART_DATA.reduce((prevWorkoutData, workoutData) => {
-  return prevWorkoutData.thisMonth >= workoutData.thisMonth ? prevWorkoutData : workoutData;
-}).time;
+import { useGetCompletedPlanCount } from '@/lib/react-query/useCompletedPlanCount';
+import type { CompletedPlanCount } from '@/mocks/summaries/completedPlanCount/data';
+import { accessTokenAtom } from '@/stores/atoms/accessTokenAtom';
+import { modalTypeAtom } from '@/stores/atoms/modalTypeAtom';
 
 export const CompletedPlanCountByTimeChart = () => {
+  const [accessToken, setAccessToken] = useAtom(accessTokenAtom);
+  const setModalType = useSetAtom(modalTypeAtom);
+  const { data } = useGetCompletedPlanCount(accessToken, setAccessToken, setModalType);
+
+  if (!data) {
+    return null;
+  }
+
+  const { completedPlanCountByTime: completedPlanCountByTimeData } = data as CompletedPlanCount;
+
+  const MOST_WORKOUT_TIME = completedPlanCountByTimeData.reduce((prevWorkout, workout) => {
+    return prevWorkout.thisMonth >= workout.thisMonth ? prevWorkout : workout;
+  }).time;
+
   return (
     <section className="flex w-full flex-col items-center bg-gray-6 py-4">
       <p className="pb-4 text-center text-xs">
         주로 <span className="text-primary">{MOST_WORKOUT_TIME}</span>에 운동하시네요!
       </p>
-      <BarChart width={350} height={300} data={BAR_CHART_DATA}>
+      <BarChart width={350} height={300} data={completedPlanCountByTimeData}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="time" />
         <YAxis />
