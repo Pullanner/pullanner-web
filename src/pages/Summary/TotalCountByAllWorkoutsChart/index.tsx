64b1d495c6bcf9b5bbd64f20d5ded9a4d428a5f1
soralee <sora2821@gmail.com>
2023-11-11 15:05:22 +0900

    [#121] Refactor: modify step id to workoutName in totalWorkoutData using WORKOUT_NAME
    - workout을 나타내는 속성이 step, name 등이 있으므로 의미를 구체화하기 위해 workout 속성을 workoutName으로 변경

--- src/pages/Summary/TotalCountByAllWorkoutsChart/index.tsx
+++ src/pages/Summary/TotalCountByAllWorkoutsChart/index.tsx
@@ -1,6 +1,7 @@
 import { useAtom, useSetAtom } from 'jotai';
 import { PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart } from 'recharts';
 
+import { WORKOUT_NAME } from '@/constants';
 import { useGetTotalWorkoutCount } from '@/lib/react-query/useTotalWorkoutCount';
 import { accessTokenAtom } from '@/stores/atoms/accessTokenAtom';
 import { modalTypeAtom } from '@/stores/atoms/modalTypeAtom';
@@ -14,20 +15,29 @@ export const TotalCountByAllWorkoutsChart = () => {
     return null;
   }
 
-  const { totalCountByWorkout: totalCountByWorkoutData } = data;
+  const { totalCountByWorkout: totalCountByWorkoutStepData } = data;
 
-  const mostCommonWorkoutName = totalCountByWorkoutData.reduce((prevWorkoutData, workoutData) => {
-    return prevWorkoutData.totalCount >= workoutData.totalCount ? prevWorkoutData : workoutData;
-  }).workout;
+  const totalCountByWorkoutNameData = totalCountByWorkoutStepData.map(({ step, totalCount }) => {
+    return {
+      workoutName: WORKOUT_NAME[step],
+      totalCount,
+    };
+  });
+
+  const mostCommonWorkoutName = totalCountByWorkoutNameData.reduce(
+    (prevWorkoutData, workoutData) => {
+      return prevWorkoutData.totalCount >= workoutData.totalCount ? prevWorkoutData : workoutData;
+    },
+  ).workoutName;
 
   return (
     <section className="flex w-full flex-col items-center bg-gray-6 py-4">
       <p className="text-center text-xs">
         <span className="text-primary">{mostCommonWorkoutName}</span>을 가장 많이 하셨어요!
       </p>
-      <RadarChart outerRadius={70} width={350} height={230} data={totalCountByWorkoutData}>
+      <RadarChart outerRadius={70} width={350} height={230} data={totalCountByWorkoutNameData}>
         <PolarGrid />
-        <PolarAngleAxis dataKey="workout" style={{ fontSize: '14px', color: '#505050' }} />
+        <PolarAngleAxis dataKey="workoutName" style={{ fontSize: '14px', color: '#505050' }} />
         <PolarRadiusAxis angle={30} style={{ fontSize: '12px' }} />
         <Radar
           name="봉철이"
