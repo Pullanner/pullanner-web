57c8dc36b1b9974e43ac93b81e9cb4cd105708c0
soralee <sora2821@gmail.com>
2023-11-11 14:59:35 +0900

    [#121] Refactor: change workout name to step id in mock data
    - step을 1 ~ 8을 가진 union type으로 제한하기 위해, typeof 연산자로 타입을 추론하는 대신 TotalWorkoutCount 타입을 선언함

--- src/mocks/summaries/totalWorkoutCount/data.ts
+++ src/mocks/summaries/totalWorkoutCount/data.ts
@@ -1,14 +1,19 @@
-export const TOTAL_WORKOUT_COUNT_DATA = {
-  totalCountByWorkout: [
-    { workout: 'Hanging', totalCount: 600 },
-    { workout: 'Jumping Pull-up', totalCount: 500 },
-    { workout: 'Band Pull-up', totalCount: 300 },
-    { workout: 'Chin-up', totalCount: 200 },
-    { workout: 'Pull-up', totalCount: 100 },
-    { workout: 'Chest to Bar', totalCount: 50 },
-    { workout: 'Archer Pull-up', totalCount: 0 },
-    { workout: 'Muscle up', totalCount: 0 },
-  ],
+export type TotalWorkoutCount = {
+  totalCountByWorkout: {
+    step: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+    totalCount: number;
+  }[];
 };
 
-export type TotalWorkoutCount = typeof TOTAL_WORKOUT_COUNT_DATA;
+export const TOTAL_WORKOUT_COUNT_DATA: TotalWorkoutCount = {
+  totalCountByWorkout: [
+    { step: 1, totalCount: 600 },
+    { step: 2, totalCount: 500 },
+    { step: 3, totalCount: 300 },
+    { step: 4, totalCount: 200 },
+    { step: 5, totalCount: 100 },
+    { step: 6, totalCount: 50 },
+    { step: 7, totalCount: 0 },
+    { step: 8, totalCount: 0 },
+  ],
+};
