d73bcec3f85d9386c3db5934d0c873f1b93e7494
soralee <sora2821@gmail.com>
2023-11-29 11:47:03 +0900

    [#105] Refactor: make the types of possiblePullUps and impossiblePullUps same

--- src/utils/getImpossiblePullUps.ts
+++ src/utils/getImpossiblePullUps.ts
@@ -1,5 +1,7 @@
-export const getImpossiblePullUps = (possiblePullUps: number[]) => {
-  const allPullUps = [1, 2, 3, 4, 5, 6, 7, 8];
+import { StepIdForWorkout } from '@/types/workout';
+
+export const getImpossiblePullUps = (possiblePullUps: StepIdForWorkout[]) => {
+  const allPullUps: StepIdForWorkout[] = [1, 2, 3, 4, 5, 6, 7, 8];
 
   return allPullUps.filter((pullUp) => {
     return !possiblePullUps.includes(pullUp);
