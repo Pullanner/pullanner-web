8ee7ee27802274731a672a56af82cb2d4835fc0d
soralee <sora2821@gmail.com>
2023-12-21 17:25:17 +0900

    [#128] Feat: mock articles data

---
+++ src/mocks/articles/data.ts
@@ -0,0 +1,500 @@
+export const ARTICLES_DATA = {
+  totalArticles: 15,
+  articles: [
+    {
+      id: 1,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-11-27T14:48:00.000Z',
+      category: 'articles',
+      title: '풀업 어케하나요...',
+      content: '',
+      imageUrl: '',
+      likes: 4,
+      views: 10,
+      comments: [
+        {
+          id: 2,
+          commenter: '나나',
+          commenterProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '초보 화이팅',
+          likes: 2,
+          replies: [
+            {
+              id: 3,
+              commenter: '김봉철',
+              commenterProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '멋있어용',
+              likes: 2,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 4,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-11-28T14:48:00.000Z',
+      category: 'studies',
+      title: '풀업 스터디 모집합니다...',
+      content: '',
+      imageUrl: '',
+      likes: 5,
+      views: 15,
+      comments: [
+        {
+          id: 5,
+          commenter: '나나',
+          commenterProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '스터디 참여하겠습니다',
+          likes: 1,
+          replies: [
+            {
+              id: 6,
+              commenter: '봉철',
+              commenterProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '좋아용',
+              likes: 1,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 7,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-11-29T14:48:00.000Z',
+      category: 'articles',
+      title: '풀업 어케하나요...',
+      content: '',
+      imageUrl: '',
+      likes: 1,
+      views: 20,
+      comments: [
+        {
+          id: 8,
+          commenter: '나나',
+          commenterProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '초보 화이팅',
+          likes: 2,
+          replies: [
+            {
+              id: 9,
+              commenter: '김봉철',
+              commenterProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '멋있어용',
+              likes: 2,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 10,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-11-28T14:48:00.000Z',
+      category: 'studies',
+      title: '머슬업 스터디 모집합니다...',
+      content: '',
+      imageUrl: '',
+      likes: 2,
+      views: 5,
+      comments: [
+        {
+          id: 11,
+          author: '나나',
+          authorProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '저도 참여하겠습니다',
+          likes: 1,
+          replies: [
+            {
+              id: 12,
+              author: '봉철',
+              authorProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '좋아용',
+              likes: 1,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 13,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-12-02T14:48:00.000Z',
+      category: 'articles',
+      title: '머슬업 어케하나요...',
+      content: '',
+      imageUrl: '',
+      likes: 3,
+      views: 8,
+      comments: [
+        {
+          id: 14,
+          author: '나나',
+          authorProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '초보 화이팅',
+          likes: 2,
+          replies: [
+            {
+              id: 15,
+              author: '김봉철',
+              authorProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '멋있어용',
+              likes: 2,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 15,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-12-03T14:48:00.000Z',
+      category: 'articles',
+      title: '풀업 어케하나요...',
+      content: '',
+      imageUrl: '',
+      likes: 8,
+      views: 11,
+      comments: [
+        {
+          id: 2,
+          commenter: '나나',
+          commenterProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '초보 화이팅',
+          likes: 2,
+          replies: [
+            {
+              id: 3,
+              commenter: '김봉철',
+              commenterProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '멋있어용',
+              likes: 2,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 16,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-12-04T14:48:00.000Z',
+      category: 'studies',
+      title: '풀업 스터디 모집합니다...',
+      content: '',
+      imageUrl: '',
+      likes: 7,
+      views: 12,
+      comments: [
+        {
+          id: 5,
+          commenter: '나나',
+          commenterProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '스터디 참여하겠습니다',
+          likes: 1,
+          replies: [
+            {
+              id: 6,
+              commenter: '봉철',
+              commenterProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '좋아용',
+              likes: 1,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 17,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-12-05T14:48:00.000Z',
+      category: 'articles',
+      title: '풀업 어케하나요...',
+      content: '',
+      imageUrl: '',
+      likes: 6,
+      views: 13,
+      comments: [
+        {
+          id: 8,
+          commenter: '나나',
+          commenterProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '초보 화이팅',
+          likes: 2,
+          replies: [
+            {
+              id: 9,
+              commenter: '김봉철',
+              commenterProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '멋있어용',
+              likes: 2,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 18,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-12-06T14:48:00.000Z',
+      category: 'studies',
+      title: '머슬업 스터디 모집합니다...',
+      content: '',
+      imageUrl: '',
+      likes: 5,
+      views: 23,
+      comments: [
+        {
+          id: 11,
+          author: '나나',
+          authorProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '저도 참여하겠습니다',
+          likes: 1,
+          replies: [
+            {
+              id: 12,
+              author: '봉철',
+              authorProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '좋아용',
+              likes: 1,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 19,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-12-07T14:48:00.000Z',
+      category: 'articles',
+      title: '머슬업 어케하나요...',
+      content: '',
+      imageUrl: '',
+      likes: 4,
+      views: 22,
+      comments: [
+        {
+          id: 14,
+          author: '나나',
+          authorProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '초보 화이팅',
+          likes: 2,
+          replies: [
+            {
+              id: 15,
+              author: '김봉철',
+              authorProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '멋있어용',
+              likes: 2,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 20,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-12-08T14:48:00.000Z',
+      category: 'articles',
+      title: '풀업 어케하나요...',
+      content: '',
+      imageUrl: '',
+      likes: 10,
+      views: 15,
+      comments: [
+        {
+          id: 2,
+          commenter: '나나',
+          commenterProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '초보 화이팅',
+          likes: 2,
+          replies: [
+            {
+              id: 3,
+              commenter: '김봉철',
+              commenterProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '멋있어용',
+              likes: 2,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 21,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-12-09T14:48:00.000Z',
+      category: 'studies',
+      title: '풀업 스터디 모집합니다...',
+      content: '',
+      imageUrl: '',
+      likes: 11,
+      views: 16,
+      comments: [
+        {
+          id: 5,
+          commenter: '나나',
+          commenterProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '스터디 참여하겠습니다',
+          likes: 1,
+          replies: [
+            {
+              id: 6,
+              commenter: '봉철',
+              commenterProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '좋아용',
+              likes: 1,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 22,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-12-10T14:48:00.000Z',
+      category: 'articles',
+      title: '풀업 어케하나요...',
+      content: '',
+      imageUrl: '',
+      likes: 12,
+      views: 17,
+      comments: [
+        {
+          id: 8,
+          commenter: '나나',
+          commenterProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '초보 화이팅',
+          likes: 2,
+          replies: [
+            {
+              id: 9,
+              commenter: '김봉철',
+              commenterProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '멋있어용',
+              likes: 2,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 23,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-12-11T14:48:00.000Z',
+      category: 'studies',
+      title: '머슬업 스터디 모집합니다...',
+      content: '',
+      imageUrl: '',
+      likes: 13,
+      views: 13,
+      comments: [
+        {
+          id: 11,
+          author: '나나',
+          authorProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '저도 참여하겠습니다',
+          likes: 1,
+          replies: [
+            {
+              id: 12,
+              author: '봉철',
+              authorProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '좋아용',
+              likes: 1,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 24,
+      author: '봉철',
+      authorProfileImage:
+        'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+      createdAt: '2023-12-12T14:48:00.000Z',
+      category: 'articles',
+      title: '머슬업 어케하나요...',
+      content: '',
+      imageUrl: '',
+      likes: 11,
+      views: 12,
+      comments: [
+        {
+          id: 14,
+          author: '나나',
+          authorProfileImage:
+            'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+          content: '초보 화이팅',
+          likes: 2,
+          replies: [
+            {
+              id: 15,
+              author: '김봉철',
+              authorProfileImage:
+                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80',
+              content: '멋있어용',
+              likes: 2,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
