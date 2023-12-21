c6cfc9eceb4cb275d1a547ffb4d506813fb70d70
soralee <sora2821@gmail.com>
2023-12-21 22:29:03 +0900

    [#128] Feat: add CommunityTab component, edit image to Community page

--- src/pages/Community/index.tsx
+++ src/pages/Community/index.tsx
@@ -2,12 +2,21 @@ import { Banner } from '@/components/Banner';
 import { BANNER_DATA } from '@/constants';
 
 import { CommunityBoard } from './CommunityBoard';
+import { CommunityTabs } from './CommunityTabs';
 
 export const Community = () => {
   return (
     <>
       <Banner data={BANNER_DATA.community} />
       <div className="px-4 pt-5">
+        <div className="flex items-center justify-between pt-5">
+          <CommunityTabs />
+          <img
+            className="h-5 w-5"
+            src="/assets/images/article-edit-icon.png"
+            alt="article-edit-icon"
+          />
+        </div>
         <CommunityBoard />
       </div>
     </>
