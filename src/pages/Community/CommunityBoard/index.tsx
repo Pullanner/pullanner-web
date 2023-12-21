9e9ae4ef50931eff206f609cbe34abc3f77b499f
soralee <sora2821@gmail.com>
2023-12-21 22:09:49 +0900

    [#128] Feat: add CommunityBoard component
    - useGetAllArticles 함수 호출을 통해 얻은 데이터를 ArticleList 컴포넌트로 렌더링함

---
+++ src/pages/Community/CommunityBoard/index.tsx
@@ -0,0 +1,35 @@
+import { useAtom, useSetAtom } from 'jotai';
+import { useSearchParams } from 'react-router-dom';
+
+import { useGetAllArticles } from '@/lib/react-query/useArticles';
+import { ArticleList } from '@/pages/Community/ArticleList';
+import { accessTokenAtom } from '@/stores/atoms/accessTokenAtom';
+import { modalTypeAtom } from '@/stores/atoms/modalTypeAtom';
+
+export const CommunityBoard = () => {
+  const [searchParams, _] = useSearchParams();
+  const [accessToken, setAccessToken] = useAtom(accessTokenAtom);
+  const setModalType = useSetAtom(modalTypeAtom);
+  const { data: articleData } = useGetAllArticles(
+    accessToken,
+    setAccessToken,
+    setModalType,
+    searchParams,
+  );
+
+  if (!articleData) {
+    return null;
+  }
+
+  const { articles } = articleData;
+
+  return (
+    <section>
+      <ul className="border-t-2 border-gray-4">
+        {articles.map((article) => {
+          return <ArticleList key={article.id} article={article} />;
+        })}
+      </ul>
+    </section>
+  );
+};
--- src/pages/Community/index.tsx
+++ src/pages/Community/index.tsx
@@ -1,13 +1,14 @@
 import { Banner } from '@/components/Banner';
 import { BANNER_DATA } from '@/constants';
 
+import { CommunityBoard } from './CommunityBoard';
+
 export const Community = () => {
   return (
     <>
       <Banner data={BANNER_DATA.community} />
-      <div className="mt-12 flex flex-col items-center p-5 text-lg">
-        <p>Community 페이지는 현재 준비중입니다.</p>
-        <p>조금만 기다려 주세요!</p>
+      <div className="px-4 pt-5">
+        <CommunityBoard />
       </div>
     </>
   );
